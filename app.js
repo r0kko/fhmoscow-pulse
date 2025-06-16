e4a9e1c83add754557c170da91a1da9d8d2ef03e
Alexey Drobot <89997389+R0kko@users.noreply.github.com>
2025-06-16 16:40:43 +0300

    Add CORS and cookie parsing

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
 import indexRouter from './src/routes/index.js';
 import requestLogger from './src/middlewares/requestLogger.js';
 import swaggerSpec from './src/docs/swagger.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cors({ origin: true, credentials: true }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(requestLogger);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
