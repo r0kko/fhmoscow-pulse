e1129454abba306ef4a7f3abfd7c6d0266d6f1c7
aadrobot <aadrobot@edu.hse.ru>
2025-06-17 16:04:39 +0300

    small lint fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -32,11 +32,9 @@ export default {
       });
     } catch (_err) {
       if (_err.message === 'account_locked') {
-        return res
-          .status(401)
-          .json({
-            error: 'Account locked due to multiple failed login attempts',
-          });
+        return res.status(401).json({
+          error: 'Account locked due to multiple failed login attempts',
+        });
       }
       void _err;
       return res.status(401).json({ error: 'Invalid credentials' });
