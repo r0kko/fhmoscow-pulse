4310fcb3ea54802e1e37f3d37037b15a8cf94671
Alexey Drobot <89997389+R0kko@users.noreply.github.com>
2025-06-24 16:52:50 +0300

    fix: prevent scroll on login page

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,12 +6,15 @@ import FooterBar from './components/FooterBar.vue'
 
 const route = useRoute()
 const showLayout = computed(() => route.path !== '/login')
+const mainClass = computed(() =>
+  showLayout.value ? 'flex-grow-1 container py-3' : 'flex-grow-1'
+)
 </script>
 
 <template>
   <div class="d-flex flex-column min-vh-100">
     <NavBar v-if="showLayout" />
-    <main class="flex-grow-1 container py-3">
+    <main :class="mainClass">
       <router-view />
     </main>
     <FooterBar v-if="showLayout" />
