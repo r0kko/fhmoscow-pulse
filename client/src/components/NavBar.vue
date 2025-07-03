80080878c1b769717b5619e55f64c1e7942778ab
Alexey Drobot <89997389+R0kko@users.noreply.github.com>
2025-07-03 18:13:01 +0300

    Update navbar responsiveness

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,26 +1,18 @@
 <template>
-  <nav class="navbar navbar-expand-md navbar-dark" style="background-color: var(--brand-color)">
+  <nav class="navbar navbar-dark" style="background-color: var(--brand-color)">
     <div class="container-fluid">
       <RouterLink class="navbar-brand d-flex align-items-center gap-2" to="/">
         <img :src="logo" alt="FHM" height="30" />
         Отдел организации судейства
       </RouterLink>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <span class="navbar-text me-3" v-if="user">
+      <div class="d-flex align-items-center ms-auto">
+        <span class="navbar-text me-3 d-none d-md-inline" v-if="user">
           {{ user.last_name }} {{ user.first_name }} {{ user.patronymic }}
         </span>
-        <button class="btn btn-outline-light" @click="logout">Выйти</button>
+        <button class="btn btn-outline-light d-none d-md-inline" @click="logout">Выйти</button>
+        <button class="btn btn-outline-light d-md-none" @click="logout">
+          <i class="bi bi-box-arrow-right"></i>
+        </button>
       </div>
     </div>
   </nav>
