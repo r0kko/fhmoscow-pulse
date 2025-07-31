dcebf7d9f0b36529610da035c477baa1ce357dbe
Alexey Drobot <89997389+r0kko@users.noreply.github.com>
2025-07-31 11:04:18 +0300

    Record online normative result on approval

--- src/seeders/20250917023000-add-normative-ticket-type.js
+++ src/seeders/20250917023000-add-normative-ticket-type.js
@@ -5,8 +5,9 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = {
   async up(queryInterface) {
     const now = new Date();
+    // prettier-ignore
     const [existing] = await queryInterface.sequelize.query(
-      "SELECT COUNT(*) AS cnt FROM ticket_types WHERE alias = 'NORMATIVE_ONLINE';"
+      'SELECT COUNT(*) AS cnt FROM ticket_types WHERE alias = \'NORMATIVE_ONLINE\';'
     );
     if (Number(existing[0].cnt) > 0) return;
     await queryInterface.bulkInsert(
