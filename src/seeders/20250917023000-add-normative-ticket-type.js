7ffc06be3b8642535d2aa44c011a244bf2cf8c5a
aadrobot <aadrobot@edu.hse.ru>
2025-08-01 14:46:05 +0300

    fixes

--- src/seeders/20250917023000-add-normative-ticket-type.js
+++ src/seeders/20250917023000-add-normative-ticket-type.js
@@ -5,9 +5,9 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = {
   async up(queryInterface) {
     const now = new Date();
-    // prettier-ignore
     const [existing] = await queryInterface.sequelize.query(
-      'SELECT COUNT(*) AS cnt FROM ticket_types WHERE alias = \'NORMATIVE_ONLINE\';'
+      // eslint-disable-next-line
+      "SELECT COUNT(*) AS cnt FROM ticket_types WHERE alias = 'NORMATIVE_ONLINE';"
     );
     if (Number(existing[0].cnt) > 0) return;
     await queryInterface.bulkInsert(
