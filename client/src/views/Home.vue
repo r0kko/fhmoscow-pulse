3da2265de3c30d0b31a32377e20ad0731b069616
Alexey Drobot <89997389+r0kko@users.noreply.github.com>
2025-08-16 20:01:53 +0300

    Move qualification section to top of work services

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -48,7 +48,7 @@ async function checkCourse() {
 
 const workSections = computed(() =>
   hasCourse.value
-    ? [...baseWorkSections, qualificationSection]
+    ? [qualificationSection, ...baseWorkSections]
     : baseWorkSections
 );
 
