ebfe367eaf91a1dec966d3add7cce8c513491ea3
aadrobot <aadrobot@edu.hse.ru>
2025-08-17 09:38:40 +0300

    another try to fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,34 @@
 export default {
-  testEnvironment: 'node',
-  transform: {},
-  maxWorkers: 1,
-  resetModules: true,
-  clearMocks: true,
-  restoreMocks: true,
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageThreshold: {
-    global: {
-      statements: 70,
-      branches: 55,
-      functions: 70,
-      lines: 70,
+    testEnvironment: 'node',
+    transform: {},
+
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '/dist/',
+        '/build/',
+        '<rootDir>/client/',
+    ],
+
+    modulePathIgnorePatterns: [
+        '<rootDir>/client/node_modules',
+        '<rootDir>/dist',
+        '<rootDir>/build',
+    ],
+
+    roots: ['<rootDir>/src', '<rootDir>/tests'],
+
+    maxWorkers: 1,
+    resetModules: true,
+    clearMocks: true,
+    restoreMocks: true,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageThreshold: {
+        global: {
+            statements: 70,
+            branches: 55,
+            functions: 70,
+            lines: 70,
+        },
     },
-  },
 };
